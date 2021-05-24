38e3f69b2193c29a30ec9cd0e1efef8ee00dad49
Erik Fasterius <erik.fasterius@outlook.com>
2021-05-24 16:41:16 +0200

    Add script for uploading pages to Canvas

---
+++ pages/upload-page-to-canvas.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# Script that takes a markdown file as input and creates or updates a page for
+# the course based on its filename, as applicable. The markdown document to be
+# uploaded should be supplied as the first argument to the script. The title of
+# the upload can be changed in Canvas after the first upload to something more
+# suitable, which won't affect subsequent updates.
+
+# Parameters
+MARKDOWN=$1
+COURSE_ID=51980
+TOKEN="$HOME/.canvas-api-token"
+API="https://uppsala.instructure.com/api/v1/courses"
+PAGE=$(basename $MARKDOWN | sed 's/.md//g')
+HTML=$(basename $MARKDOWN | sed 's/.md/.html/g')
+
+# Convert using Pandoc
+docker run --rm \
+    --volume "`pwd`:/data" \
+    --user `id -u`:`id -g` \
+    pandoc/latex $MARKDOWN -o $HTML
+
+# Check if current page already exists
+curl -X GET \
+    "$API/$COURSE_ID/pages/$PAGE" \
+    --header "Authorization: Bearer $(cat $TOKEN)" \
+    --silent --show-error \
+    > /dev/null
+
+# Create or update page, as applicable
+if [ $? -eq 0 ]; then
+    echo "Page \`$PAGE\` already exists: updating it"
+    curl -X PUT \
+        "$API/$COURSE_ID/pages/$PAGE" \
+        --header "Authorization: Bearer $(cat ~/.canvas-api-token)" \
+        --data wiki_page[body]="$(cat $HTML)" \
+        --silent --show-error \
+        > /dev/null
+else
+    echo "Page \`$PAGE\` does not exists: creating it"
+    curl -X POST \
+        "$API/$COURSE_ID/pages/$PAGE" \
+        --header "Authorization: Bearer $(cat ~/.canvas-api-token)" \
+        --data wiki_page[title]="$PAGE" \
+        --data wiki_page[body]="$(cat $HTML)" \
+        --silent --show-error \
+        > /dev/null
+fi
+
+# Delete rendered HTML
+rm $HTML
