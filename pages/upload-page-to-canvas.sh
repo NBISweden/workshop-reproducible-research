47d21565b582178de6d2e1547639bf20d4b4ae20
Erik Fasterius <erik.fasterius@outlook.com>
2021-09-30 11:49:45 +0200

    Try using pipes instead of slashes for sed

--- pages/upload-page-to-canvas.sh
+++ pages/upload-page-to-canvas.sh
@@ -33,11 +33,11 @@ fi
 
 # General parameters
 API="https://uppsala.instructure.com/api/v1/courses"
-PAGE=$(basename $MARKDOWN | sed 's/.md//g')
-HTML=$(basename $MARKDOWN | sed 's/.md/.html/g')
+PAGE=$(basename $MARKDOWN | sed 's|.md||g')
+HTML=$(basename $MARKDOWN | sed 's|.md|.html/g')
 
 # Get current branch and build GitHub address
-BRANCH=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+BRANCH=$(git branch | sed -n -e 's|^\* \(.*\)|\1|p')
 GITHUB="https:\/\/raw\.githubusercontent\.com\/NBISweden\/workshop-reproducible-research\/$BRANCH\/pages\/"
 
 # Get the appropriate course ID from the current branch
@@ -56,9 +56,9 @@ docker run --rm \
 
 # Add images from GitHub and course ID for links
 cat "$HTML" \
-    | sed "s/\(src=\"\)\(images\/\)/\1$GITHUB\2/g" \
-    | sed "s/COURSE_ID/$COURSE_ID/g" \
-    | sed "s/GITHUB_BRANCH/$BRANCH/g" \
+    | sed "s|\(src=\"\)\(images\/\)|\1$GITHUB\2|g" \
+    | sed "s|COURSE_ID|$COURSE_ID|g" \
+    | sed "s|GITHUB_BRANCH|$BRANCH|g" \
     > tmp.html
 echo '<div class="container">' | cat - tmp.html > tmp2.html
 echo "</div>" >> tmp2.html
