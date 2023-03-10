0dc5d5170017dad896c090d457bda5c7e32169bd
John <john.sundh@scilifelab.se>
2023-03-10 08:51:24 +0100

    Remove sra-tools dependency from run_qc.sh scripts

--- tutorials/containers/run_qc.sh
+++ tutorials/containers/run_qc.sh
@@ -3,10 +3,15 @@ mkdir -p data/raw_internal
 mkdir -p intermediate/fastqc
 mkdir -p results/fastqc
 
-# Download fastq files using sra-tools and put in data/raw_internal/:
-fastq-dump SRR935090 -X 15000 --gzip -Z > data/raw_internal/SRR935090.fastq.gz
-fastq-dump SRR935091 -X 15000 --gzip -Z > data/raw_internal/SRR935091.fastq.gz
-fastq-dump SRR935092 -X 15000 --gzip -Z > data/raw_internal/SRR935092.fastq.gz
+# Define URLs to fastq files for each sample
+SRR935090=""
+SRR935091=""
+SRR935092=""
+
+# Download fastq files from remote repository and put in data/raw_internal/:
+curl -L  "$SRR935090" -o data/raw_internal/SRR935090.fastq.gz
+curl -L  "$SRR935091" -o data/raw_internal/SRR935091.fastq.gz
+curl -L  "$SRR935092" -o data/raw_internal/SRR935092.fastq.gz
 
 # Run fastqc, put output zip in intermediate/fastq/ and html in results/fastqc:
 fastqc  data/raw_internal/SRR935090.fastq.gz --outdir=intermediate/fastqc/
