661cc6b8a5db2a479310ec46496336221b770d5a
John Sundh <john.sundh@scilifelab.se>
2023-04-27 14:16:19 +0200

    Revert "Fix my_docker_conda build"
    This reverts commit 8bce8dbf46376cc4950af61037df31b73616e8d4.

--- tutorials/containers/run_qc.sh
+++ tutorials/containers/run_qc.sh
@@ -9,9 +9,9 @@ SRR935091="https://figshare.scilifelab.se/ndownloader/files/39539770"
 SRR935092="https://figshare.scilifelab.se/ndownloader/files/39539773"
 
 # Download fastq files from remote repository and put in data/raw_internal/:
-wget "$SRR935090" -O data/raw_internal/SRR935090.fastq.gz
-wget "$SRR935091" -O data/raw_internal/SRR935091.fastq.gz
-wget "$SRR935092" -O data/raw_internal/SRR935092.fastq.gz
+curl -L  "$SRR935090" -o data/raw_internal/SRR935090.fastq.gz
+curl -L  "$SRR935091" -o data/raw_internal/SRR935091.fastq.gz
+curl -L  "$SRR935092" -o data/raw_internal/SRR935092.fastq.gz
 
 # Run fastqc, put output zip in intermediate/fastq/ and html in results/fastqc:
 fastqc  data/raw_internal/SRR935090.fastq.gz --outdir=intermediate/fastqc/
